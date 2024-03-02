845a5c32ead8040007fb9d973035e5dbdfb1c7f2
Wesley Bruno <engwesleybruno@gmail.com>
2024-03-02 20:38:03 -0300

    feat: ok

--- lib/components/card/index.tsx
+++ lib/components/card/index.tsx
@@ -1,9 +1,5 @@
 import { CardProps } from "./types";
 
 export function Card(props: CardProps) {
-  return (
-    <div className={`w-full rounded overflow-hidden shadow-md p-4`}>
-      {props.children}
-    </div>
-  );
+  return <div className={`w-full rounded shadow-md p-4`}>{props.children}</div>;
 }
