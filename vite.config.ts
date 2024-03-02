ebca5cad2791e14be50008976650acab4352ef67
Wesley Bruno <engwesleybruno@gmail.com>
2024-03-02 10:34:58 -0300

    feat:ok

--- vite.config.ts
+++ vite.config.ts
@@ -8,21 +8,18 @@ import { extname, relative, resolve } from "path";
 import { fileURLToPath } from "node:url";
 import { glob } from "glob";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
   build: {
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
       input: Object.fromEntries(
-        glob.sync("lib/**/*.{ts,tsx}").map((file) => [
-          // The name of the entry point
-          // lib/nested/foo.ts becomes nested/foo
-          relative("lib", file.slice(0, file.length - extname(file).length)),
-          // The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url)),
-        ])
+        glob
+          .sync("lib/**/*.{ts,tsx}")
+          .map((file) => [
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
       ),
       output: {
         assetFileNames: "assets/[name][extname]",
