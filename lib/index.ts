c0d8de930dff3ba252f1912ca83edc0b1c87b728
Leandro Sousa <49341725+leopsousaa@users.noreply.github.com>
2024-03-18 09:47:00 -0300

    feat: adicionado exortação das funções utilitárias

--- lib/index.ts
+++ lib/index.ts
@@ -3,22 +3,24 @@ import "./index.css";
 export { DashboardLayout } from "./layouts/dashboard-layout";
 
 export { Button } from "./components/button";
-export { Input } from "./components/input";
-export { Popover } from "./components/popover";
-export { Spinner } from "./components/spinner";
 export { Card } from "./components/card";
-export { Select } from "./components/select";
+export { BarChart } from "./components/charts/bar-chart";
+export { DougnutChart } from "./components/charts/dougnut-chart";
+export { LineChart } from "./components/charts/line-chart";
+export { PieChart } from "./components/charts/pie-chart";
 export { Checkbox } from "./components/checkbox";
-export { RadioButtonGroup } from "./components/radio-button";
+export { Input } from "./components/input";
+export { InputMask } from "./components/input-mask";
 export { Modal } from "./components/modal";
+export { Popover } from "./components/popover";
+export { RadioButtonGroup } from "./components/radio-button";
 export { RichText } from "./components/rich-text";
+export { Select } from "./components/select";
+export { Spinner } from "./components/spinner";
 export { Tabs } from "./components/tabs";
 export { Wizard } from "./components/wizard";
-export { InputMask } from "./components/input-mask";
-export { BarChart } from "./components/charts/bar-chart";
-export { LineChart } from "./components/charts/line-chart";
-export { DougnutChart } from "./components/charts/dougnut-chart";
-export { PieChart } from "./components/charts/pie-chart";
 
+export { Table } from "./components/table";
 export { showToast } from "./components/toast/showToast";
-export {Table} from "./components/table"
+
+export * from "./utils";
