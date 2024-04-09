ff59f0ccb089119a18097414b44c754a67599767
Leandro Sousa <49341725+leopsousaa@users.noreply.github.com>
2024-04-09 15:06:28 -0300

    feat: atualização das importações

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
+export * from "./assets";
 export * from "./components";
 export * from "./layouts";
 export * from "./utils";
