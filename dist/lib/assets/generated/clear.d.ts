6907e80344ea0a48d50fdadae331437388b26c44
Leandro Sousa <49341725+leopsousaa@users.noreply.github.com>
2024-04-14 13:17:44 -0300

    feat: build button v2

---
+++ dist/lib/assets/generated/clear.d.ts
@@ -0,0 +1,20 @@
+export declare const clear: {
+    tag: string;
+    props: {
+        xmlns: string;
+        viewBox: string;
+    };
+    children: ({
+        tag: string;
+        props: {
+            fill: string;
+            d: string;
+        };
+    } | {
+        tag: string;
+        props: {
+            d: string;
+            fill?: undefined;
+        };
+    })[];
+};
--- dist/lib/components/buttonV2/index.d.ts
+++ dist/lib/components/buttonV2/index.d.ts
@@ -1,17 +1,4 @@
-/// <reference types="react" />
+import { Ref } from "react";
+import { ButtonProps } from "./types";
 import "./styles.css";
-export declare const ButtonV2: import("react").ForwardRefExoticComponent<import("react-aria").AriaButtonProps<"button"> & {
-    isLink?: boolean | undefined;
-    isVertical?: boolean | undefined;
-    isTogglable?: boolean | undefined;
-    isOutlined?: boolean | undefined;
-    isSummary?: boolean | undefined;
-    variant?: import("../..").ThemeVariant | undefined;
-    title?: string | undefined;
-    onToggle?: ((state: import("react-stately").ToggleState) => void) | undefined;
-    size?: import("../..").ThemeSize | undefined;
-    className?: string | undefined;
-    isLoading?: boolean | undefined;
-} & {
-    children?: import("react").ReactNode;
-} & import("react").RefAttributes<HTMLButtonElement>>;
+export declare function ButtonV2(props: ButtonProps, ref: Ref<HTMLButtonElement>): import("react/jsx-runtime").JSX.Element;
