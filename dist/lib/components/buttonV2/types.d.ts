8b21a02555863b343b1f919e9ab9315ccc2b8740
Leandro Sousa <49341725+leopsousaa@users.noreply.github.com>
2024-04-14 13:42:28 -0300

    fix: build application

--- dist/lib/components/buttonV2/types.d.ts
+++ dist/lib/components/buttonV2/types.d.ts
@@ -1,16 +1,13 @@
 import type { PropsWithChildren } from "react";
-import type { ToggleState } from "@react-stately/toggle";
 import type { AriaButtonProps } from "@react-types/button";
 import { ThemeSize, ThemeVariant } from "../..";
 export type ButtonProps = AriaButtonProps<"button"> & PropsWithChildren<{
     isLink?: boolean;
     isVertical?: boolean;
-    isTogglable?: boolean;
     isOutlined?: boolean;
     isSummary?: boolean;
     variant?: ThemeVariant;
     title?: string;
-    onToggle?: (state: ToggleState) => void;
     size?: ThemeSize;
     className?: string;
     isLoading?: boolean;
